e156d43ce3fd27e93a928de9528f8a1333d8b1be
Ankur Sardar <ankursardar.work@gmail.com>
2025-11-15 04:00:27 +0530

    header masti done

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
+    <div className='w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4'>
         <div>
       <Image src={assets.profile_img} alt='' className='rounded-full w-32'/>
         </div>
@@ -14,9 +14,17 @@ const Header = () => {
       </h3>
       <h1 className='text-3xl sm-text-6xl lg:text-[66px] font-Ovo'>
         frontend web developer baed in London.</h1>
-        <p>
+        <p className='max-w-2xl mx-auto font-Ovo'>
             I'm a frontend developer from California, USA with 10 years of experience in multiple companies like Microsoft, Tesla and Apple.
         </p>
+        <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
+            <a href="contact"
+            className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2'>
+              contact me <Image src={assets.right_arrow_white} alt='' className='w-4'/></a>
+            <a href="/sample-resume.pdf" download
+            className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 '>
+                my resume <Image src={assets.download_icon} alt='' className='w-4'/></a>
+        </div>
     </div>
   )
 }
