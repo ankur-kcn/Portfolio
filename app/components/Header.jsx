d020669e13ee95c0258fa8690ab0c04f931ad073
Ankur Sardar <ankursardar.work@gmail.com>
2025-11-15 03:24:20 +0530

    Navbar masti done

---
+++ app/components/Header.jsx
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+import { assets } from '../../assets/assets'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <div>
+      <Image src={assets.profile_img} alt='' className='rounded-full w-32'/>
+        </div>
+         <h3 className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo'>
+        Hi! I'm William Mark
+      <Image src={assets.hand_icon} alt='' className=' w-6'/>
+      </h3>
+      <h1 className='text-3xl sm-text-6xl lg:text-[66px] font-Ovo'>
+        frontend web developer baed in London.</h1>
+        <p>
+            I'm a frontend developer from California, USA with 10 years of experience in multiple companies like Microsoft, Tesla and Apple.
+        </p>
+    </div>
+  )
+}
+
+export default Header
